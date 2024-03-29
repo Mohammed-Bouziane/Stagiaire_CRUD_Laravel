485c1335b9c06a0800a96d31dc63f8fdb0d5fe95
Mohammed-Bouziane <mohamed.officiel.fr@gmail.com>
2024-03-29 12:00:32 +0000

    nothing in particular to mention

--- database/seeders/StagiairesSeeder.php
+++ database/seeders/StagiairesSeeder.php
@@ -2,11 +2,10 @@
 
 namespace Database\Seeders;
 
+use App\Models\Stagiaire;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
-use Illuminate\Support\Str;
-use App\Models\Stagiaire;
 
 class StagiairesSeeder extends Seeder
 {
@@ -15,22 +14,23 @@ class StagiairesSeeder extends Seeder
      */
     public function run(): void
     {
-    
-        Stagiaire::factory()->count(50)->create();
-        
-    }
-}
+        //! using for loop
+        // for ($i = 0; $i < 10; $i++) {
+        //     DB::table('stagiaires')->insert([
+        //         'nom' => fake()->name,
+        //         'prenom' => fake()->name,
+        //         'email' => fake()->email,
+        //         'adresse' => fake()->city,
 
-// for ($i = 0; $i < 100; $i++) {
-//     DB::table("stagiaires")->insert([
-//         'nom' => fake()->name,
-//         'prenom' => fake()->name,
-//         'email' =>fake()->email,
-//         'adresse' => fake()->city,
-//     ]);
-// }
-//         // 'nom' => Str::random(10),
-//         // 'prenom' => Str::random(10),
-//         // 'email' => Str::random(10) . '@gmail.com',
-//         // 'email' =>fake()->safeEmail,
-//         // 'adresse' => Str::random(20),
+        //! using Str and random functions
+        // 'nom' => Str::random(10),
+        // 'prenom' => Str::random(10),
+        // 'email' => Str::random(10) . '@gmail.com',
+        // 'email' =>fake()->safeEmail,
+        // 'adresse' => Str::random(20),
+        //     ]);
+        // }
+        Stagiaire::factory()->count(10)->create();
+    }
+
+};
